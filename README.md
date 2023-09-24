26f63559c98bba8a61d96f58cd75fce169e32057
Noza23 <G.Nozadze@campus.lmu.de>
2023-09-24 13:12:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# TransferSAM
+
+[![LMU: Munich](https://img.shields.io/badge/LMU-Munich-009440.svg)](https://www.en.statistik.uni-muenchen.de/index.html)
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+AutoTestGen uses Large Language Models to analyze code segments and autonomously generate unit tests.
+It was developed as part of the **Practical Applications for Large Language Models** seminar at LMU Munich under the guidance and supervision of Martin Binder.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='AutoTestGen',
+    url='https://github.com/LMU-Seminar-LLMs/AutoTestGen',
+    author='Giorgi Nozadze',
+    author_email='giorginozadze23@yahoo.com',
+    long_description=long_description,
+    packages=find_packages(exclude="notebooks"),
+    # Needed for dependencies
+    install_requires=[],
+    version='0.1',
+    license='MIT',
+    description='Automatic Unit Test generation using LLM',
+    python_requires='>=3.9'
+)
